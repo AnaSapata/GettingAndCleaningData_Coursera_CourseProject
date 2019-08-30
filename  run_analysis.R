ed04de8bbc153c8e43835de57b87bc2a750264cc
AnaSapata <ana_sapata@sapo.pt>
2019-08-30 12:22:01 +0100

    Add the run_analysis.R file and update the README.md

---
+++  run_analysis.R
@@ -0,0 +1,70 @@
+# ------------------------------------------------------------------------------------------------------------------------------- #
+#                                                                                                                                 #
+#                                     Getting and Cleaning Data - Coursera - Course Project                                       #
+#                                                         30/Ago/2018                                                             #
+#                                               Ana Sapata (ana_sapata@sapo.pt)                                                   #
+#                                                                                                                                 #
+# ------------------------------------------------------------------------------------------------------------------------------- #
+
+
+# Download the zip
+download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip',
+              '/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/Dataset.zip')
+
+# Load the test sets
+x_test <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/test/X_test.txt')
+y_test <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/test/y_test.txt')
+subject_test <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/test/subject_test.txt')
+
+# Merge of all datasets related with test set
+test <- cbind(subject_test, x_test, y_test)
+
+# Load the train sets
+x_train <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/train/X_train.txt')
+y_train <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/train/y_train.txt')
+subject_train <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/train/subject_train.txt')
+
+# Merge of all datasets related with train set
+train <- cbind(subject_train, x_train, y_train)
+
+# Merge the data from train set and test set
+all_data <- rbind(train, test)
+
+# load the file with the features names
+features <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/features.txt',
+                       stringsAsFactors = FALSE)
+
+# Attribute the names of features in the previous file to the columns of the data frame with all the data
+colnames(all_data) <- c("Subject", features[,2], "Activity")
+
+# Get the index of columns with "-mean()" in the name
+mean_index <- grep("-mean()", colnames(all_data))
+
+# Get the index of columns with "-std()" in the name
+std_index <- grep("-std()", colnames(all_data))
+
+# From all data just get the variables Subject, Activity and all related with mean and std
+index_columns <- c(1, mean_index, std_index, 563)
+final_data <- all_data[,sort(index_columns)]
+
+# Load the file with the activities name
+activities <- read.table('/home/anasapata/Personal/datasciencecoursera/Getting and Cleaning Data/Week4/UCI HAR Dataset/activity_labels.txt',
+                         stringsAsFactors = FALSE)
+
+final_data_w_act <- merge(final_data, activities, by.x = "Activity", by.y = "V1")
+# Remove the variable activity since is integer type
+final_data_w_act$Activity <- NULL
+# Rename the columns, so V2 become Activity
+colnames(final_data_w_act) <- c(colnames(final_data_w_act[,-81]), "Activity")
+# Get the index of columns with "-mean()" in the name
+mean_index_2 <- grep("-mean()", colnames(final_data_w_act))
+# Just the mean data
+final_data_w_act_mean <- final_data_w_act[, c(1, mean_index_2, 81)]
+
+library(tidyr)
+# Get the data in tidy form
+tidy_data <- gather(final_data_w_act_mean, variable, mean, -Activity, -Subject)
+# Separate the variable with the name "variable" into variable, measure and axis
+tidy_data <- separate(data = tidy_data, col = variable, into = c("variable", "measure", "axis"))
+# Delete the variable measure, since is repeate
+tidy_data$measure <- NULL
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# GettingAndCleaningData_Coursera_CourseProject
+## GettingAndCleaningData_Coursera_CourseProject
+
+The script start to download the zip file. After that I unzip the same manually. 
+
+It is loaded the test sets (*X_test*, *y_test*, *subject_test*), and then the train sets (*X_training*, *y_trainig*, *subject_train*). To merge all the test sets it was used the **cbind**, to put all the columns together, the same was made for the train sets.
+
+Since the test and train sets has the same structure(all variables in the same order) was used the **rbind** to merge the two data sets. Since the data is now all together the next step was attribute the names to the variables(columns). Their names are in the file *features.txt*, which was readed with the function **read.table**.
+
+The second column of the previous data frame was assigned to the columns names which index was between 2 and 562, and the first variable take the name *"Subject"* and the last *"Activity"*.
+
+Following that, searched for the columns referents to the mean and standard deviation, throught the **grep** function using the patterns *"-mean()"* and *"-std()"*, getting the respective column index. The index was combined in a vector and was obtained a subset by this index, plus the 1 and 563. Now, I has a data frame with the subject, the variables corresponding to the mean and standard deviation, and the activity.
+
+Since the activity is code by the numbers 1-6, I read the file *activity_labels.txt* to decode this. Was made a merge throught the previous data frame and the activity data frame, by the columns with the numbers, after that, was deleted the column with the code of the activity and rename the columns with the descriptive activity to *"Activity"*.
+
+Once in the tidy data just was need the mean values, was made again the grep with the pattern *"-mean()"* to get the index of the columns that corresponding to the mean. After, was obtained a subset from the data frame, to have just the columns *Subject*, *Activity* and the corresponding to the mean.
+
+Was used the **gather** function to transform the data in tidy data, and also used the function **separate** to obtain the *variable*, *measure* and *axis*, from the previous variable that contain all this three together. Since the *measure* is always mean and the last column that contais the values is also called *mean*, the *measure* column was deleted. 
+
+#### Now I get the tidy data that was expect :D
